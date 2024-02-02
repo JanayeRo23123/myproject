63e99378e5c5c079b13bfb856cf6f63e72e2bdb2
JanayeRo23123 <janaye.rouser1@gmail.com>
2024-02-01 19:38:06 -0600

    107.2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/navbar';
+import Navbar from './components/footer';
+import QuantityPicker from './components/quantityPicker';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello From React and the Server!</h1>
+      <Navbar/>
+      <Footer/>
+      <QuantityPicker/>
+
+      
     </div>
   );
 }
